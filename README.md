071618e23aa6190595fd51660d2480583316ac5d
yunajoe <yunajoe@gmail.com>
2026-02-15 14:38:21 +0900

    feat: e.nativeEvent 로 수정

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 미션을 통해 알게 된점
 
+## 1. e.target.value의 속성의 사용
+
 - e.target.value 속성은 사용자가 데이터를 입력하거나 선택하는 form 요소 태그에 있다. 가장 대표적인 태그는 input, textarea, select, button, option
 
 - form 요소 태그가 아닌 태그에서 value를 가져오고 싶다면은
@@ -29,3 +31,51 @@
 >
 ```
 ````
+
+## 2. 엔터 이벤트가 2번 발생하는 현상
+
+이유는 대부분 한글입력(IME, 조합입력) 때문이다.
+한글을 입력할 때는 내부적으로 아래와 같은 흐름이 실행이 된다.
+
+```md
+1. onCompositionStart → 조합 시작 (예: ㅎ + ㅏ)
+2. onKeyDown (Enter)
+3. onCompositionEnd → 글자 확정
+4. onKeyDown (Enter) 한 번 더 실행되는 경우가 있음
+
+즉, 한글 입력 상태에서 Enter를 누르면:
+조합을 끝내기 위한 Enter
+실제 Enter 입력
+```
+
+어떻게 해결할까?
+
+1. input 속성의 onCompositionStart, onCompositionEnd 속성을 추가하여 state 로 관리한다.
+
+```javascript
+const [isComposing, setIsComposing] = useState(false);
+
+<input
+  value={addItem}
+  onKeyDown={handleKeyDown}
+  onChange={(e) => {
+    setAddItem(e.target.value);
+  }}
+  onCompositionStart={() => setIsComposing(true)}
+  onCompositionEnd={() => setIsComposing(false)}
+/>;
+```
+
+2. React 에서는 KeyboardEvent 안에 이미 nativeEvent.isComposing속성이 있다.
+
+```javascript
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" && !e.nativeEvent.isComposing) {
+      setFruitsList((prev) => {
+        return [...prev, addItem];
+      });
+      setAddItem("");
+    }
+  };
+
+```
--- src/Fruits.tsx
+++ src/Fruits.tsx
@@ -1,8 +1,11 @@
-import { MouseEvent } from "react";
+import { KeyboardEvent, MouseEvent, useState } from "react";
 
 const fruitsArray = ["사과", "바나나", "수박", "멜론", "키위"];
 
 function Fruits() {
+  const [fruitsList, setFruitsList] = useState<string[]>(fruitsArray);
+  const [addItem, setAddItem] = useState("");
+  const [clickedItem, setClickedItem] = useState("");
   const handleClick = (
     fruit: string,
     index: number,
@@ -12,21 +15,57 @@ function Fruits() {
         인덱스: ${index}
         SyntheticEvent 객체: ${e}
     `;
-    console.log(log);
+    setClickedItem(fruit);
+  };
+
+  const handleAddItem = () => {
+    // 버튼에는 e.target.value가 없낭
+    setFruitsList((prev) => {
+      return [...prev, addItem];
+    });
+    setAddItem("");
+  };
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" && !e.nativeEvent.isComposing) {
+      setFruitsList((prev) => {
+        return [...prev, addItem];
+      });
+      setAddItem("");
+    }
   };
 
   return (
-    <ul>
-      {fruitsArray.map((fruit, index) => (
-        <li
-          key={index}
-          onClick={(event) => handleClick(fruit, index, event)}
-          data-fruit={fruit}
-        >
-          <p>아이템: {fruit}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      <div style={{ display: "flex", columnGap: "10px" }}>
+        <input
+          value={addItem}
+          onKeyDown={handleKeyDown}
+          onChange={(e) => {
+            setAddItem(e.target.value);
+          }}
+        />
+        <button onClick={handleAddItem} disabled={!addItem}>
+          과일 추가하기
+        </button>
+      </div>
+
+      <div>
+        <h3>과일 리스트</h3>
+        <ul>
+          {fruitsList.map((fruit, index) => (
+            <li
+              key={index}
+              style={{ background: fruit === clickedItem ? "blue" : "" }}
+              onClick={(event) => handleClick(fruit, index, event)}
+              data-fruit={fruit}
+            >
+              <p>아이템: {fruit}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
