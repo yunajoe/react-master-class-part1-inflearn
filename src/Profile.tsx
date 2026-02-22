e715bd9e5470f81b73a58fdf958dfa4d6fa51f20
yunajoe <yunajoe@gmail.com>
2026-02-22 19:51:19 +0900

    feat: mission

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -25,42 +25,116 @@ setUser({ ...user, city: "부산" });
 
  */
 
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 function Profile() {
-  const [wrongProfile, setWrongProfile] = useState({
-    name: "철수",
-    age: 20,
-  });
+  const [data, setData] = useState([
+    {
+      id: 1,
+      name: "철수",
+      age: 20,
+      city: "서울",
+    },
+    {
+      id: 2,
+      name: "묭이",
+      age: 25,
+      city: "부산",
+    },
+    {
+      id: 3,
+      name: "뜌뜌",
+      age: 30,
+      city: "수원",
+    },
+    {
+      id: 4,
+      name: "영희",
+      age: 20,
+      city: "용산",
+    },
+  ]);
+  const [newCityArr, setNewCityArr] = useState([
+    { id: 1, city: "" },
+    { id: 2, city: "" },
+    { id: 3, city: "" },
+    { id: 4, city: "" },
+  ]);
 
-  const [correctProfile, setCorrectProfile] = useState({
-    name: "철수",
-    age: 20,
-  });
-  const handleWrongProfile = () => {
-    // 기존 객체를 수정해서 그대로 넣기
-    wrongProfile.age += 1;
-    setWrongProfile(wrongProfile);
-  };
-  const handleCorrectProfile = () => {
-    setCorrectProfile({
-      ...correctProfile,
-      age: correctProfile.age + 1,
+  const handleIncreaseAge = (id: number) => {
+    setData((prev) => {
+      return prev.map((item) =>
+        item.id === id ? { ...item, age: item.age + 1 } : item,
+      );
     });
   };
+
+  const handleChangeCity = (
+    e: ChangeEvent<HTMLInputElement, HTMLInputElement>,
+    targetId: number,
+  ) => {
+    setNewCityArr((prev) => {
+      return prev.map((item) =>
+        item.id === targetId ? { ...item, city: e.target.value } : item,
+      );
+    });
+  };
+  const handleUpdateCity = (targetId: number) => {
+    const updateCityName = newCityArr.find(
+      (item) => item.id === targetId,
+    )?.city;
+    if (!updateCityName) return;
+    setData((prev) => {
+      return prev.map((item) =>
+        item.id === targetId ? { ...item, city: updateCityName } : item,
+      );
+    });
+  };
+
   return (
     <div>
-      <div>
-        <p>
-          이름:{wrongProfile.name} - {wrongProfile.age}
-        </p>
-        <button onClick={handleWrongProfile}>잘못된 버튼</button>
-      </div>
-      <div>
-        <p>
-          이름:{correctProfile.name} - {correctProfile.age}
-        </p>
-        <button onClick={handleCorrectProfile}>올바른 버튼</button>
+      <div style={{ display: "flex" }}>
+        <div>
+          {data.map((item) => (
+            <div
+              key={item.id}
+              style={{
+                border: "5px solid green",
+                display: "flex",
+                columnGap: "10px",
+              }}
+            >
+              <p>이름: {item.name}</p>
+              <p>나이: {item.age}</p>
+              <p>도시: {item.city}</p>
+              <button onClick={() => handleIncreaseAge(item.id)}>
+                나이 증가 버튼
+              </button>
+            </div>
+          ))}
+        </div>
+        <div
+          style={{
+            border: "5px solid red",
+            display: "flex",
+            flexDirection: "column",
+            rowGap: "20px",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {newCityArr.map((item) => (
+            <div key={item.id} style={{ display: "flex", columnGap: "10px" }}>
+              <input
+                value={item.city}
+                onChange={(e) => handleChangeCity(e, item.id)}
+              />
+              <button onClick={() => handleUpdateCity(item.id)}>
+                도시업데이트
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
